f58468d428c4c6f82cae4c2d026361d1d459716d
Cristina Jung <cristijung@gmail.com>
2022-11-10 20:19:49 -0300

    usando recoil

--- src/state/hook/useAdicionarParticipante.ts
+++ src/state/hook/useAdicionarParticipante.ts
@@ -1,13 +1,21 @@
 //hook personalizado usando os hooks do recoil
 //para usá-lo, basta importar onde for preciso
 
-import { listaParticipantesState } from "../atom"
-import { useSetRecoilState } from 'recoil';
+import { listaParticipantesState, erroState } from "../atom"
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
 
 
 export const useAdicionarParticipante = () => {
     const setLista = useSetRecoilState(listaParticipantesState)
-    return (nomeDoParticipante: string) => {
+    const lista = useRecoilValue(listaParticipantesState)
+    const setErro = useSetRecoilState(erroState)
+   
+    return (nomeDoParticipante: string) => { //criando a funcionalidade de duplicidade por meio de hooks personalizados + condicionais
+        if (lista.includes(nomeDoParticipante)) {
+            setErro('Nomes duplicados não são permitidos')
+            return
+        }
         return setLista(listaAtual => [...listaAtual, nomeDoParticipante])
     }
 }
