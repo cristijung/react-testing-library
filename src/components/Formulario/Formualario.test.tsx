4800f0bf52a23d69414a306cd872fea644fc80b8
Cristina Jung <cristijung@gmail.com>
2022-11-10 16:06:28 -0300

    implementando testes na aplicação

--- src/components/Formulario/Formualario.test.tsx
+++ src/components/Formulario/Formualario.test.tsx
@@ -1,10 +1,15 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 import Formulario from './Formulario';
 
-
+//teste 1
 test('quando o input está vazio, novos participantes não podem ser add', () => {
     //o teste será feito na renderização do componente formulario
-    render(<Formulario />)
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>
+        )
     //encontrar no DOM o elemento Input que contenha o placeholder 'Insira ....'
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
     //encontrar o botão que possui a regra de button
@@ -15,19 +20,55 @@ test('quando o input está vazio, novos participantes não podem ser add', () =>
     expect(botao).toBeDisabled()
 })
 
-// test('adicionar um participante caso exista um nome preenchido', () => {
-//     render(<Formulario />)
-//     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-//     const botao = screen.getByRole('button')
-//     //inserir um valor no input usano um helper do testing-library para disparar um evento do input
-//     //o valor do target é uma simulação
-//     fireEvent.change(input, {
-//         target: {
-//             value: 'Josefina'
-//         }
-//     })
-//     //clicar no botão
+//teste 2
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>
+        )
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const botao = screen.getByRole('button')
+    //inserir um valor no input usano um helper do testing-library para disparar um evento do input
+    //o valor do target é uma simulação
+    fireEvent.change(input, {
+        target: {
+            value: 'Josefina'
+         }
+     })
+     //clicar no botão
+     fireEvent.click(botao)
+     //garantir que o input estea com o foco ativo - fazer o teste
+     expect(input).toHaveFocus()
+     //precisamos garantir que o input não tenha um valor
+     expect(input).toHaveValue('')
+ })
 
+ //teste 3
+ test('nomes duplicados não podem ser adicionados na lista', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>
+        )
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const botao = screen.getByRole('button')   
+    //no teste, clicamos no input, vai  o nome, clica no botão e depois repete
+    //novamente
+    fireEvent.change(input, {
+        target: {
+            value: 'Josefina'
+         }
+     })     
+     fireEvent.click(botao)
+     fireEvent.change(input, {
+        target: {
+            value: 'Josefina'
+         }
+     })     
+     fireEvent.click(botao)
 
-// })
+     const mensagemDeErro = screen.getByRole('alert')
+    expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos')
+ })
 
--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,10 +1,31 @@
+import React, { useRef, useState } from "react"
+import { useAdicionarParticipante } from "../../state/hook/useAdicionarParticipante"
 
 const Formulario = () => {
+    
+    const [nome, setNome] = useState('')
+   
+    const inputRef = useRef<HTMLInputElement>(null) 
+
+    const adicionarNaLista = useAdicionarParticipante()
+    const adicionarParticipante = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault() 
+        adicionarNaLista(nome) 
+        setNome('') 
+        inputRef.current?.focus()    
+    }    
+    
     return (
-        <>  
-        <form>
-            <input type='text' placeholder='Insira os nomes dos participantes' />
-            <button disabled={true}>Adicionar</button>
+        <>      
+        <form onSubmit={adicionarParticipante}>
+            <input 
+                ref={inputRef}
+                value={nome}
+                onChange={e => setNome(e.target.value)}
+                type='text' 
+                placeholder='Insira os nomes dos participantes' 
+            />
+            <button disabled={!nome}>Adicionar</button>
         </form>
         </>
     )
